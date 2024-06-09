724defa6eac85ceb115d8bc05d10b4bdace76768
Thomas Van <thomascvan11@gmail.com>
2024-06-09 12:52:19 -0700

    add functionality to submit data to chain

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Layout, Row, Col, Button } from "antd";
+import { Layout, Row, Col, Button, Spin } from "antd";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import {
+  useWallet,
+  InputTransactionData,
+} from "@aptos-labs/wallet-adapter-react";
 
 // By default, Aptos will interact with the devnet network, to set up a different network, we can use AptosConfig class.
 // import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
@@ -12,11 +15,16 @@ import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { Aptos } from "@aptos-labs/ts-sdk";
 
 const aptos = new Aptos();
+export const moduleAddress =
+  "95741305186a8c4430f50dadc4fe6f1e5ea90f3c7832914cf7cc131b2dd80f39";
 
 function App() {
-  const { account } = useWallet();
+  const { account, signAndSubmitTransaction } = useWallet();
 
   const [accountHasList, setAccountHasList] = useState<boolean>(false);
+
+  const [transactionInProgress, setTransactionInProgress] =
+    useState<boolean>(false);
 
   useEffect(() => {
     fetchList();
@@ -25,8 +33,6 @@ function App() {
   const fetchList = async () => {
     if (!account) return [];
     // change this to be your module account address
-    const moduleAddress =
-      "95741305186a8c4430f50dadc4fe6f1e5ea90f3c7832914cf7cc131b2dd80f39";
     try {
       const todoListResource = await aptos.getAccountResource({
         accountAddress: account?.address,
@@ -38,31 +44,47 @@ function App() {
     }
   };
 
+  const addNewList = async () => {
+    if (!account) return [];
+    setTransactionInProgress(true);
+    const transaction: InputTransactionData = {
+      data: {
+        function: `${moduleAddress}::todolist::create_list`,
+        functionArguments: [],
+      },
+    };
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(transaction);
+      // wait for transaction
+      await aptos.waitForTransaction({ transactionHash: response.hash });
+      setAccountHasList(true);
+    } catch (error: any) {
+      setAccountHasList(false);
+    } finally {
+      setTransactionInProgress(false);
+    }
+  };
+
   return (
     <>
-      <Layout>
-        <Row align="middle">
-          <Col span={10} offset={2}>
-            <h1>Our todolist</h1>
-          </Col>
-          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
-            <WalletSelector />
-          </Col>
-        </Row>
-      </Layout>
-      {!accountHasList && (
-        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
-          <Col span={8} offset={8}>
-            <Button
-              block
-              type="primary"
-              style={{ height: "40px", backgroundColor: "#3f67ff" }}
-            >
-              Add new list
-            </Button>
-          </Col>
-        </Row>
-      )}
+      ...
+      <Spin spinning={transactionInProgress}>
+        {!accountHasList && (
+          <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+            <Col span={8} offset={8}>
+              <Button
+                onClick={addNewList}
+                block
+                type="primary"
+                style={{ height: "40px", backgroundColor: "#3f67ff" }}
+              >
+                Add new list
+              </Button>
+            </Col>
+          </Row>
+        )}
+      </Spin>
     </>
   );
 }
